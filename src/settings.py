99d5e6077cad1e90d8fca45e1e100204d5b8fd2d
skorodenko <72010957+skorodenko@users.noreply.github.com>
2024-07-03 23:22:06 +0300

    Fixed settings load

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import toml
 from xdg_base_dirs import xdg_config_home, xdg_data_home
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -13,25 +13,26 @@ APP_CONFIG = xdg_config_home() / "ksol"
 APP_DATA = xdg_data_home() / "ksol"
 
 
+SETTINGS_FIELS = [str(APP_DATA / "settings.default.toml")]
+
+
 class MPDSettings(BaseModel):
-    # socket: str | None = Field(None)
-    socket: str = Field(str(APP_DATA / "mpd.socket"))
-    native_socket: str = Field(str(APP_DATA / "mpd.socket"))
-    native_config: str = Field(str(APP_CONFIG / "mpd.conf"))
+    socket: str
+    native_socket: str
+    native_config: str
 
 
 class Settings(BaseSettings):
-    mpd: MPDSettings = MPDSettings()
-    model_config = SettingsConfigDict(toml_file=["./settings.default.toml"])
-    
+    mpd: MPDSettings
+    model_config = SettingsConfigDict(toml_file=SETTINGS_FIELS)
+
     def commit(self, output_file: str):
         parsed = self.model_dump()
         config_toml = toml.dumps(parsed)
         with open(output_file, "w") as f:
             f.write(config_toml)
 
-    def rollback(self):
-        ...
+    def rollback(self): ...
 
     @classmethod
     def settings_customise_sources(
@@ -46,4 +47,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
