22ba10332f06f4d4e89e55e14432ea0c84eee537
skorodenko <mskorodenko@gmail.com>
2025-09-29 17:50:19 +0300

    ...

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rust devshell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=210c7e4d8d958a8f373a97aada2c40579cd2020d";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
@@ -28,15 +28,21 @@
           mkShell {
             buildInputs = [
               mpd
-              clang
               qt6.full
-              kdePackages.qtdeclarative
+              kdePackages.extra-cmake-modules
+              kdePackages.kirigami
+              kdePackages.kirigami-addons
+              kdePackages.qqc2-desktop-style
+              kdePackages.wrapQtAppsHook
+              clang
+              cmake
               llvmPackages.bintools
               rust-analyzer
               rust-bin.beta.latest.default
             ];
             shellHook = ''
-              export QML_IMPORT_PATH=$NIXPKGS_QT6_QML_IMPORT_PATH
+              #export QML_IMPORT_PATH=$NIXPKGS_QT6_QML_IMPORT_PATH
+              #export QML2_IMPORT_PATH=${qt6.qtdeclarative}/lib/qt-6/qml:${kdePackages.qtdeclarative}/lib/qt-6/qml
               export QMAKE=qmake6
               export RUST_LOG=DEBUG
             '';
